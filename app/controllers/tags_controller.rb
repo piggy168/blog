20b134ca8afd0f4de33d2bcfceeb168f0d1bf524
jim <jim@boomgamez.com>
2016-06-07 15:49:10 -0700

    auth

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,4 +1,12 @@
 class TagsController < ApplicationController
+  before_filter :zero_authors_or_authenticated, only: [:destroy]
+
+  def zero_authors_or_authenticated
+    unless Author.count == 0 || current_user
+    redirect_to root_path
+      return false
+    end
+  end
   def show
   @tag = Tag.find(params[:id])
   end
